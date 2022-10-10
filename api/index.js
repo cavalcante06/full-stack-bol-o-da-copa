6ae66d035895463b9455815cf9d5b7ed426c470c
César augusto <104602302+cavalcante06@users.noreply.github.com>
2022-10-09 23:02:42 -0300

    iniciando o back end

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log (10+10)
+import Koa from 'koa'
+import Router from '@Koa/router'
+
+
+const app = new Koa ();
+const router = new Router ()
+
+router.get('/', async ctx => {
+ctx.body = { ola:"MUndo"} 
+
+})
+
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
