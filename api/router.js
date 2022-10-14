fd83cb6b93b647cd62045224f628a020edd7f10f
César augusto <104602302+cavalcante06@users.noreply.github.com>
2022-10-13 23:34:36 -0300

    back end

--- api/router.js
+++ api/router.js
@@ -1,23 +1,9 @@
 import Router from '@Koa/router'
 
+import  * as users from './app/users/index.js'
+
 export const router = new Router()
 
-const users = []
-
-router.get ('/users', async ctx => {
-ctx.body = users
-
-})
-
-router.post('/users', async ctx => {
-    const user = {
-        username: ctx.request.body.username
-    } 
-
-    users.push (user)
-
-    ctx.body = user
-    })
-
-   
+router.post('/users', users.create)
+router.get('/users', users.list)   
     
